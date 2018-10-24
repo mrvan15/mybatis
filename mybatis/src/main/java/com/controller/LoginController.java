8829a4d41337f3ef28c268a2978ffaaaacc36173
mrvan15 <woshifanguannan@163.com>
2018-10-25 00:57:43 +0800

    commit1 mymy

---
+++ mybatis/src/main/java/com/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.User;
+
+@RestController
+public class LoginController {
+	
+	@RequestMapping("login")
+	public String login(String name,String pwd,HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		
+		if(name.equals("root")&&pwd.equals("root")) {
+            User user = new User();
+            user.setName(name);
+            session.setAttribute("user",user);
+            return "登录成功";
+        } else {
+            return "用户名或密码错误!";
+        }
+		
+	}
+
+}
